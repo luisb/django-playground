27ee968a757a8212321ac243d6fe38a91d0d862a
Luis Baquera <luisb@ucr.edu>
2024-07-10 15:46:32 -0700

    Create edit view; modify create Post interface to edit Posts

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='posts'),
     path('post/create', views.create_post, name='post-create'),
+    path('post/edit/<int:id>/', views.edit_post, name='post-edit'),
     path('about/', views.about, name='about'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import Post
 from .forms import PostForm
 
 # Create your views here.
+def edit_post(request, id):
+    post = get_object_or_404(Post, id=id)
+
+    if request.method == 'GET':
+        context = {
+            'form': PostForm(instance=post), 
+            'id': id
+        }
+        return render(request, 'blog/post_form.html', context)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'The post has been updated successfully.')
+            return redirect('posts')
+        else:
+            messages.error(request, 'Please correct the following errors:')
+            return render(request,'blog/post_form.html',{'form':form})
+
 def create_post(request):
     if request.method == 'GET':
         context = {
